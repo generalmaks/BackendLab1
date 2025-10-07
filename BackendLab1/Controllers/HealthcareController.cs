ac251ea1f9d2e92f9d09aa6d95ef328519e206b8
generalmaks <schrinan@gmail.com>
2025-10-07 21:31:22 +0300

    Added healthcare controller and removed useless code

---
+++ BackendLab1/Controllers/HealthcareController.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BackendLab1.Controllers;
+
+[ApiController]
+public class HealthcareController: Controller
+{
+    [HttpGet("/healthcheck")]
+    public async Task<IActionResult> GetHealthcare()
+    {
+        object result = new
+        {
+            Status = nameof(HealthStatus.Healthy),
+            Time = DateTime.Now.ToString(CultureInfo.CurrentCulture)
+        };
+            
+        return Ok(result);
+    }
+}
